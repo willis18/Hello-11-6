e556fbecb5de093090fb9560c3bf299fbc4f4fe9
willis18 <willis18@naver.com>
2021-02-10 08:20:42 +0900

    11-6 까지해서 완료

--- Hello1/ViewController.swift
+++ Hello1/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblNumber: UILabel!
+    var x = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnUp(_ sender: Any)
+    {
+        x=x+1
+        lblNumber.text = String(x)
+    }
+    
+    @IBAction func btnDown(_ sender: UIButton)
+    {
+        x=x-1
+        lblNumber.text = String(x)
+    }
 }
 
